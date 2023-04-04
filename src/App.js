0a801d1b60e70aae0aef559ea26ed13c89131a64
Eren-Enou <erenenou@yahoo.com>
2023-04-04 08:33:37 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import Form from './components/Form';
 import ToDoDisplay from './components/To-Do_Display';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 function App() {
@@ -16,11 +17,20 @@ function App() {
   return (
     <div className="App">
       <Nav username={name} city={"Cotati"}/>
-      <Form addTask={addTask} />
-      <div>
-        <h1>Tasks!</h1>
-      </div>
-      <ToDoDisplay tasks={tasks} />
+      
+      <Container className="text-center">
+        <Row className="justify-content-center">
+          <Col md={6}>
+            <Form addTask={addTask} />
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+        <Col md={8}>
+          <div><h1>Tasks!</h1></div>
+          <ToDoDisplay tasks={tasks} />
+        </Col>
+      </Row>
+      </Container>
     </div>
   );
 }
